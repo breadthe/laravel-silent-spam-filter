84070c67a4342c33f4c988d23a7c70ea98118218
breadthe <omigoshdev@protonmail.com>
2019-05-20 04:05:57 +0000

    Apply fixes from StyleCI

--- src/SilentSpam.php
+++ src/SilentSpam.php
@@ -7,7 +7,7 @@ class SilentSpam
     protected $blacklist = [];
 
     /**
-     * Amend the silentspam config blacklist at runtime with additional criteria
+     * Amend the silentspam config blacklist at runtime with additional criteria.
      *
      * @param array $keywords
      */
@@ -18,17 +18,17 @@ class SilentSpam
 
     public function isSpam($message)
     {
-        foreach(array_merge(config('silentspam.blacklist') ?? [], $this->blacklist) as $keyword) {
+        foreach (array_merge(config('silentspam.blacklist') ?? [], $this->blacklist) as $keyword) {
             if (preg_match("/{$keyword}/i", $message)) {
                 return true;
             }
-        };
+        }
 
         return false;
     }
 
     public function notSpam($message)
     {
-        return !$this->isSpam($message);
+        return ! $this->isSpam($message);
     }
 }
